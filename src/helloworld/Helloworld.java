8f558bc97fa7c63154ebc32ab00af1a7680b5e46
raohongsheng <raohongsheng>
2019-11-07 13:42:33 +0800

    test_02

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -1,6 +1,6 @@
 package helloworld;
 
-public class helloworld {
+public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("我的第一个git工程");
 		System.out.println("有人喊我改改你");
